8c14ebac0f7231b979ce21a197c10ac321f7a72d
Evgeniy <9778595931@mail.ru>
2026-02-20 21:02:13 +0300

    пофиксил баг с запуском postgres

--- django_service/config/settings.py
+++ django_service/config/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG").lower() == 'true'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "django"]
 
 
 # Application definition
